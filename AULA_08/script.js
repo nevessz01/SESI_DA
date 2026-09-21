702d5e3b120d6b32385d4d51f7b057c1b4c686a2
nevessz01 <jaomarcio79@gmail.com>
2026-09-21 12:20:49 -0300

    upd

---
+++ AULA_08/script.js
@@ -0,0 +1,11 @@
+// const nome = localStorage.getItem("nome");
+
+//akert (nome);
+
+//localStorage.setItem("nome");
+
+//alert(localStorage.getItem("nome");
+
+//localStorage.removeItem("nome");
+
+function login
